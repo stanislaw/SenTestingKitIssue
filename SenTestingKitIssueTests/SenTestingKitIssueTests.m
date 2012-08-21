0adabc988e224c30e16a637eba26bd514858d06c
stanislaw <s.pankevich@gmail.com>
2012-08-21 14:46:39 +0300

    adds second test using dispath_sync

--- SenTestingKitIssueTests/SenTestingKitIssueTests.m
+++ SenTestingKitIssueTests/SenTestingKitIssueTests.m
@@ -7,25 +7,43 @@
 
 @implementation SenTestingKitIssueTests
 
-- (void)testTestRequest
+// Both below begin to hang
+
+//- (void)testTestRequestUsingDispatchSemaphore
+//{
+//    NSLog(@"Beginning");
+//    dispatch_semaphore_t sema = dispatch_semaphore_create(0);
+//    
+//    [[APIClient sharedClient] getPath:@"timezoneJSON" parameters:nil success:^(AFHTTPRequestOperation *operation, id JSON) {
+//        NSLog(@"Success");
+//        
+//        STAssertTrue(TRUE, @"Authenticate result should be YES");
+//        
+//        dispatch_semaphore_signal(sema);
+//        
+//    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+//        NSLog(@"Error");
+//    }];
+//    
+//    dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
+//    dispatch_release(sema);
+//}
+
+- (void)testTestRequestUsingDispatchSync
 {
     NSLog(@"Beginning");
-    dispatch_semaphore_t sema = dispatch_semaphore_create(0);
-    
-    [[APIClient sharedClient] getPath:@"timezoneJSON" parameters:nil success:^(AFHTTPRequestOperation *operation, id JSON) {
-        NSLog(@"Success");
-        
-        STAssertTrue(TRUE, @"Authenticate result should be YES");
-        
-        dispatch_semaphore_signal(sema);
-        
-    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-        NSLog(@"Error");
-    }];
-    
-    dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
-    dispatch_release(sema);
 
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        [[APIClient sharedClient] getPath:@"timezoneJSON" parameters:nil success:^(AFHTTPRequestOperation *operation, id JSON) {
+            NSLog(@"\nSuccess");
+
+
+        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+            NSLog(@"\nError");
+        }];
+    });
+    
+    STAssertTrue(TRUE, @"Authenticate result should be YES");
 }
 
 @end
