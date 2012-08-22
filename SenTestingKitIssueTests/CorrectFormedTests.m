97401f4cb43f7a199108cc07661dbc26290cab94
stanislaw <s.pankevich@gmail.com>
2012-08-22 10:51:15 +0300

    adds explanation

--- SenTestingKitIssueTests/CorrectFormedTests.m
+++ SenTestingKitIssueTests/CorrectFormedTests.m
@@ -1,3 +1,10 @@
+// https://github.com/AFNetworking/AFNetworking/issues/466#issuecomment-7926896
+// The issue is that dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER); blocks the thread and waits for the signal to continue execution, however and now i have to guess .. since AFNetworking does its inner workings also in threads it the success block is executed in a dispatch_group_t which might not get thru with the semaphore blocking.
+
+// The difference is that i don't have the DISPATCH_TIME_FOREVER on the semaphore but rather DISPATCH_TIME_NOW in a while which keeps the semaphore waiting but not blocking, in addition the runloop continues to be executed causing AFNetworking to continue as intended and when the signal finally is sent to the semaphore the loop will break.
+
+// I might be totally wrong on this, but thats what i have observed so far and well it does work ;)
+
 #import <SenTestingKit/SenTestingKit.h>
 #import <APIClient.h>
 
